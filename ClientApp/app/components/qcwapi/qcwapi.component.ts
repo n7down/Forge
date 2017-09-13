f0af6f2ab6f19fed6b1fc61bee2b13852cfbde7f
Joshua Adams <n7down@gmail.com>
2017-09-12 23:35:01 -0600

    updating qcwapi files

--- ClientApp/app/components/qcwapi/qcwapi.component.ts
+++ ClientApp/app/components/qcwapi/qcwapi.component.ts
@@ -8,22 +8,22 @@ import { Http } from '@angular/http';
 export class QuadcopterWorkbenchAPIComponent {
     public forecasts: WeatherForecast[];
     public baseUrl: string;
+    public data: string = "no data";
 
     constructor(private http: Http, @Inject('BASE_URL') baseUrl: string) {
-        // http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-        //     this.forecasts = result.json() as WeatherForecast[];
-        // }, error => console.error(error));
-        console.log(baseUrl);
-        baseUrl = baseUrl;
+        this.baseUrl = baseUrl;
     }
 
     public request() {
-        // this.http.get(this.baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-        //     this.forecasts = result.json() as WeatherForecast[];
-        // }, error => console.error(error));
-        this.http.get(this.baseUrl + 'api/fc').subscribe(result => {
-            console.log(result.json());
+        this.http.get(this.baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+
+            // TODO: setup for flight controller interface
+            this.forecasts = result.json() as WeatherForecast[];
+            this.data = JSON.stringify(this.forecasts, null, 2);
         }, error => console.error(error));
+        // this.http.get(this.baseUrl + 'api/fc').subscribe(result => {
+        //     this.data = JSON.parse(result.json());
+        // }, error => console.error(error));
     }
 }
 
@@ -33,3 +33,30 @@ interface WeatherForecast {
     temperatureF: number;
     summary: string;
 }
+
+// interface FlightController {
+//     public long Id { get; set; }
+//     public string Name { get; set; }
+//     public string MCU { get; set; }
+//     public string GyroName { get; set; }
+//     public bool OSD { get; set; }
+//     public string OSDName { get; set; }
+//     public bool PDB { get; set; }
+//     public string LipoVoltage { get; set; }
+//     public bool SDCard { get; set; }
+
+//     // weight in grams
+//     public float Weight { get; set; }
+//     public int NumberUARTS { get; set; }
+//     public bool Barometer { get; set; }
+//     public bool PWM { get; set; }
+//     public bool SBUS { get; set; }
+//     public bool DSMTwo { get; set; }
+//     public bool LedStrip { get; set; }
+//     public bool VideoIn { get; set; }
+//     public bool VideoOut { get; set; }
+//     public bool Buzzer { get; set; }
+//     public int NumberSoftSerial { get; set; }
+//     public string Size { get; set; }
+//     public string MountingHoles { get; set; }
+// }
