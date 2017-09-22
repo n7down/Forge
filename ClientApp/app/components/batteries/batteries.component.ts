33fa4e6f01a70cb331d1345f51aac3bfd1028688
Joshua Adams <n7down@gmail.com>
2017-09-22 11:28:44 -0600

    updated battiers component

--- ClientApp/app/components/batteries/batteries.component.ts
+++ ClientApp/app/components/batteries/batteries.component.ts
@@ -6,5 +6,23 @@ import { Http } from '@angular/http';
     templateUrl: './batteries.component.html'
 })
 export class BatteriesComponent {
-    
+    public batteries: Batteries[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/batteries').subscribe(result => {
+            this.batteries = result.json() as Batteries[];
+        }, error => console.error(error));
+    }
 }
+
+interface Batteries {
+    Name: string;
+    LipoVoltage: string;
+    MAh: string;
+    CRating: string;
+    PlugType: string;
+    Weight: string;
+    Dimension: string;
+    Link: string;
+}
+    
