874d0d2978a731dae8822c5c68d0bdde7ff3d9d6
Joshua Adams <n7down@gmail.com>
2018-09-17 10:46:11 -0600

    bug with creating a battery

--- ClientApp/app/components/create-battery/create-battery.component.ts
+++ ClientApp/app/components/create-battery/create-battery.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 @Component({
     selector: 'create-battery',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
     styleUrls: ['./create-battery.component.css']
 })
 export class CreateBatteryComponent {
+    private sub: any;
+    private baseUrl: string;
+    private battery: Battery | undefined;
+    private id: string | undefined;
     
+    constructor(private route: ActivatedRoute, private http: Http, @Inject('BASE_URL') baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+    
+    ngOnInit() {
+    }
+
+    submit() {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let options = new RequestOptions({ headers: headers });
+        let body = JSON.stringify(this.battery);
+        this.http.post(this.baseUrl + 'api/v1/battery/', body, options).subscribe(result => {
+            console.log(result.json());
+        }, error => console.error(error));
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+}
+
+interface Battery {
+    id: string;
+    name: string;
+    lipoVoltage: string;
+    mah: number;
+    cRating: string;
+    plugType: string;
+    weight: string;
+    dimension: string;
+    link: string;
 }
